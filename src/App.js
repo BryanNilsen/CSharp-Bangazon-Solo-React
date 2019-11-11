0d74114309f048f7cd8cf202979a4fe21f052148
BryanNilsen <bryannilsen@gmail.com>
2019-11-11 09:47:24 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    customers: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/customers")
+      .then(response => response.json())
+      .then(parsed => this.setState({ customers: parsed }))
+  }
+
+
+  render() {
+
+    return (
+
+      <div className="App">
+        <header className="App-header">Bangazon API</header>
+        <h1>Customer List</h1>
+        {this.state.customers.map((customer, i) =>
+          <p key={i}>{customer.firstName} {customer.lastName}</p>
+
+        )}
+
+      </div>
+    );
+  }
 }
 
 export default App;
